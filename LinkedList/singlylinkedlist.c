1dbd09b7f7cc07bff2743cdacebee7c1d8a1633c
SANJAY A <118290917+Sanjay2729117@users.noreply.github.com>
2023-12-16 23:45:21 +0530

    Update singlylinkedlist.c

--- LinkedList/singlylinkedlist.c
+++ LinkedList/singlylinkedlist.c
@@ -1,118 +1,186 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-
-typedef struct Node{
+typedef struct Node
+{
     int data;
-    struct Node* link;
-}Node;
+    struct Node *link;
+} Node;
 
-Node* create(int data){
-    Node* newNode=(Node*)malloc(sizeof(Node*));
-    newNode->data=data;
-    newNode->link=NULL;
+Node *create(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node *));
+    newNode->data = data;
+    newNode->link = NULL;
     return newNode;
 }
 
-void insertatbeginning(Node** head,int data){
-    Node* newNode=create(data);
-    if(*head==NULL){
-        *head=newNode;
-    }else{
-    newNode->link=*head;
-    *head=newNode;
+void insertatbeginning(Node **head, int data)
+{
+    Node *newNode = create(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        newNode->link = *head;
+        *head = newNode;
     }
 }
-void insertatintermediate(Node** head,int data,int position){
-    Node* newNode=create(data);
-    Node* current=*head;
-     Node* prev=NULL;
-    int count=1;
-    while(current!=NULL){
-        if(count==position){
-           prev->link=newNode;
-           newNode->link=current;
-           break;
+void insertatintermediate(Node **head, int data, int position)
+{
+    Node *newNode = create(data);
+    Node *current = *head;
+    Node *prev = NULL;
+    int count = 1;
+    while (current != NULL)
+    {
+        if (count == position)
+        {
+            prev->link = newNode;
+            newNode->link = current;
+            break;
         }
         count++;
-        prev=current;
-         current=current->link;
+        prev = current;
+        current = current->link;
     }
 }
-void insertatlast(Node** head,int data){
-    Node* newNode=create(data);
-    Node* current=*head;
-     Node* prev=NULL;
-   while(current!=NULL){
-        prev=current;
-        current=current->link;
-    } 
-     prev->link=newNode;
+void insertatlast(Node **head, int data)
+{
+    Node *newNode = create(data);
+    Node *current = *head;
+    Node *prev = NULL;
+    while (current != NULL)
+    {
+        prev = current;
+        current = current->link;
+    }
+    prev->link = newNode;
 }
-void deleteatbeginning(Node** head){
-    Node* current=*head;
-    *head=current->link;
+void deleteatbeginning(Node **head)
+{
+    Node *current = *head;
+    *head = current->link;
     free(current);
 }
-void deleteatlast(Node** head){
-    Node* current=*head;
-    Node* prev=NULL;
-     while(current->link!=NULL){
-       
-        prev=current;
-        current=current->link;
-    } 
-    prev->link=NULL;
+void deleteatlast(Node **head)
+{
+    Node *current = *head;
+    Node *prev = NULL;
+    while (current->link != NULL)
+    {
+
+        prev = current;
+        current = current->link;
+    }
+    prev->link = NULL;
     free(current);
 }
-void deleteatintermediate(Node** head,int position){
-    Node* current=*head;
-    Node* prev=NULL;
-      int count=1;
-     while(current!=NULL){
-        if(count==position){
-           prev->link=current->link;
-           free(current);
-           break;
+void deleteatintermediate(Node **head, int position)
+{
+    Node *current = *head;
+    Node *prev = NULL;
+    int count = 1;
+    while (current != NULL)
+    {
+        if (count == position)
+        {
+            prev->link = current->link;
+            free(current);
+            break;
         }
-        prev=current;
-        current=current->link;
+        prev = current;
+        current = current->link;
         count++;
+    }
 }
-}
-void reverse(Node** head){
-    Node* current=*head;
-    Node* prev=NULL;
-    Node* next=NULL;
-    while(current!=NULL)
+void reverse(Node **head)
 {
-    next=current->link;
-    current->link=prev;
-    prev=current;
-    current=next;
+    Node *current = *head;
+    Node *prev = NULL;
+    Node *next = NULL;
+    while (current != NULL)
+    {
+        next = current->link;
+        current->link = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
 }
-    *head=prev;
-}
-void display(Node** head){
-    Node* current=*head;
-    while(current!=NULL)
+void display(Node **head)
 {
-    printf("%d ",current->data);
-    current=current->link;
-}}
-int main() {
-    Node* head=NULL;
-     insertatbeginning(&head,6);
-     insertatbeginning(&head,7);
-     insertatbeginning(&head,7);
-     insertatintermediate(&head,9,2);
-     insertatlast(&head,15);
-printf("\n");
-display(&head);
- reverse(&head);
- 
-printf("\n");
-display(&head);
+    Node *current = *head;
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->link;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    int num;
+    while (1)
+    {
+        printf("1)insert at beginning\n2)insert at intermediate\n3)insert at last\n4)delete at beginning\n5)delete at intermediate\n6)delete at last\n7)display\n8)reverse\n");
+
+        scanf("%d", &num);
+        switch (num)
+        {
+            int ele;
+            int pos;
+        case 1:
+            printf("enter a number to insert at beginning");
+            scanf("%d", &ele);
+            insertatbeginning(&head, ele);
+            break;
+        case 2:
+            printf("enter a number to insert at intermediate");
+            scanf("%d", &ele);
+            printf("enter a position to insert");
+            scanf("%d", &pos);
+            insertatintermediate(&head, ele, pos);
+            break;
+        case 3:
+            printf("enter a number to insert at last");
+            scanf("%d", &ele);
+
+            insertatlast(&head, ele);
+            break;
+        case 4:
+            deleteatbeginning(&head);
+            break;
+        case 5:
+
+            printf("enter a position to delete");
+            scanf("%d", &pos);
+            deleteatintermediate(&head, pos);
+
+            break;
+        case 6:
+            deleteatlast(&head);
+            break;
+        case 7:
+
+            printf("\n");
+            display(&head);
+            printf("\n");
+            break;
+        case 8:
+            reverse(&head);
+
+            printf("\n");
+            display(&head);
+            printf("\n");
+            break;
+        default:
+            printf("wrong number");
+            break;
+        }
+    }
 
     return 0;
 }
